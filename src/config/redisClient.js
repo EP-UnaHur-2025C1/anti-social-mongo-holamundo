fa69322a23081835572972cf3a4e2dec37016400
FrancoRueta <askjfuhje@hotmail.com>
2025-06-23 18:41:46 -0300

    solucionado error en redis, app funcional.

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,7 +1,12 @@
-const { createClient } = require('redis')
+const { createClient } = require('redis');
 
-const redisClient = createClient()
+const redisClient = createClient({
+  socket: {
+    host: process.env.REDIS_HOST || 'redis', // nombre del servicio
+    port: process.env.REDIS_PORT || 6379,
+  }
+});
 
-redisClient.on('error', (err) => console.error('Redis error: ', err))
+redisClient.on('error', (err) => console.error('Redis error: ', err));
 
-module.exports = redisClient
+module.exports = redisClient;

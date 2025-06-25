6395fb40f059cbe95f4c2d4878bcbf353318a157
FrancoRueta <askjfuhje@hotmail.com>
2025-06-25 16:00:16 -0300

    Revert "saque el redis luego lo agregamos, modifique el swagger"
    This reverts commit b035cdcb278a3705f69a54bf8bd7eb6873f7d1ec.

--- src/controllers/tags.controller.js
+++ src/controllers/tags.controller.js
@@ -4,6 +4,10 @@ const controller = {}
 const mongoose = require('../db/mongo.db').mongoose;
 
 const getTags = async (req, res) => {
+  const cachedTags = await redisClient.get("tags");
+  if (cachedTags) {
+    return res.status(200).json(JSON.parse(cachedTags));
+  }else{
   try {
     const tags = await Tag.find({}).populate('postId', {
       postId: 1,
@@ -16,14 +20,23 @@ const getTags = async (req, res) => {
     if (!tags || tags.length === 0) {
       return res.status(404).json({ message: "No se encontraron tags" });
     }
+    await redisClient.set("tags", JSON.stringify(tags), {
+      EX: 3600, // Expira en 1 hora
+      NX: true, // Solo establece si no existe
+    });
     res.status(200).json(tags);
   } catch (error) {
     res.status(500).json({ error: "Error al obtener los Tags", details: error.message });
   }
 };
+}
 controller.getTags = getTags
 const getTagById = async (req, res) => {
   const { id } = req.params;
+  const cachedTag = await redisClient.get(`tag:${id}`);
+  if (cachedTag) {
+    return res.status(200).json(JSON.parse(cachedTag));
+  }else{
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "ID inválido" });
   }
@@ -39,14 +52,20 @@ const getTagById = async (req, res) => {
     if (!tag) {
       return res.status(404).json({ error: "Tag no encontrado" });
     }
+    await redisClient.set(`tag:${id}`, JSON.stringify(tag), {
+      EX: 3600, // Expira en 1 hora
+      NX: true, // Solo establece si no existe
+    });
     res.status(200).json(tag);
   } catch (error) {
     res.status(500).json({ error: "Error al obtener el Tag", details: error.message });
   }
+}
 };
 controller.getTagById = getTagById
 const createTag = async (req, res) => {
   const { nombre, postId } = req.body;
+  
   if (!nombre || !postId) {
     return res.status(400).json({ error: "Faltan datos requeridos" });
   }
@@ -57,6 +76,8 @@ const createTag = async (req, res) => {
     }
     const newTag = new Tag({ nombre, postId });
     const savedTag = await newTag.save();
+    await redisClient.del("tags"); // Limpiar la caché de tags en Redis
+    await redisClient.del(`tag:${savedTag._id}`); // Limpiar la caché del tag recién creado en Redis
     res.status(201).json(savedTag);
   } catch (error) {
     res.status(500).json({ error: "Error al crear el Tag", details: error.message });
@@ -79,6 +100,8 @@ const updateTag = async (req, res) => {
       return res.status(404).json({ error: "Tag no encontrado" });
     }
     const updatedTag = await Tag.findByIdAndUpdate(id, { nombre, postId }, { new: true });
+    await redisClient.del("tags"); // Limpiar la caché de tags en Redis
+    await redisClient.del(`tag:${id}`); // Limpiar la caché del tag actualizado en Redis
     res.status(200).json(updatedTag);
   } catch (error) {
     res.status(500).json({ error: "Error al actualizar el Tag", details:
@@ -100,6 +123,8 @@ const deleteById = async (req, res) => {
     if (!deletedTag) {
       return res.status(404).json({ error: "Tag no encontrado para eliminar" });
     }
+    await redisClient.del("tags"); // Limpiar la caché de tags en Redis
+    await redisClient.del(`tag:${id}`); // Limpiar la caché del tag
     res.status(200).json({ message: "Tag eliminado correctamente" });
   } catch (error) {
     res.status(500).json({ error: "Error al eliminar el Tag", details: error.message });
@@ -109,6 +134,10 @@ controller.deleteById = deleteById
 
 const getPostsByTag = async (req, res) => {
   const { id } = req.params;
+  const cachedPosts = await redisClient.get(`posts:tag:${id}`);
+  if (cachedPosts) {
+    return res.status(200).json(JSON.parse(cachedPosts));
+  }else{
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "ID inválido" });
   }
@@ -120,9 +149,14 @@ const getPostsByTag = async (req, res) => {
     if (tag.postId.length === 0) {
       return res.status(404).json({ message: "No se encontraron posts para este tag" });
     }
+    await redisClient.set(`posts:tag:${id}`, JSON.stringify(tag.postId), {
+      EX: 3600, // Expira en 1 hora
+      NX: true, // Solo establece si no existe
+    });
     res.status(200).json(tag.postId);
   } catch (error) {
     res.status(500).json({ error: "Error al obtener los posts del Tag", details: error.message });
+  }
   }
 }
 controller.getPostsByTag = getPostsByTag
